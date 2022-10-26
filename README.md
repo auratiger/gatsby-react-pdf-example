fb9e218bedc345d01e49051eedd56c86cb26099d
Georgi Georgiev <gegeorgiev@vmware.com>
2022-10-26 09:26:39 +0300

    updated README and  cleaned up example

--- README.md
+++ README.md
@@ -9,21 +9,21 @@
 
 ## 🚀 Quick start
 
-1.  **Create a Gatsby site.**
-
-    Use the Gatsby CLI to create a new site, specifying the minimal starter.
+1.  **Install dependencies.**
 
     ```shell
-    # create a new Gatsby site using the minimal starter
-    npm init gatsby
+    npm install
     ```
 
-2.  **Start developing.**
-
-    Navigate into your new site’s directory and start it up.
+    or
 
     ```shell
-    cd my-gatsby-site/
+    yarn install
+    ```
+
+2.  **Run project.**
+
+    ```shell
     npm run develop
     ```
 
@@ -33,22 +33,6 @@
 
     Edit `src/pages/index.js` to see your site update in real-time!
 
-4.  **Learn more**
+## Preview
 
-    - [Documentation](https://www.gatsbyjs.com/docs/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-    - [Tutorials](https://www.gatsbyjs.com/tutorial/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-    - [Guides](https://www.gatsbyjs.com/tutorial/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-    - [API Reference](https://www.gatsbyjs.com/docs/api-reference/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-    - [Plugin Library](https://www.gatsbyjs.com/plugins?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-    - [Cheat Sheet](https://www.gatsbyjs.com/docs/cheat-sheet/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
-
-## 🚀 Quick start (Gatsby Cloud)
-
-Deploy this starter with one click on [Gatsby Cloud](https://www.gatsbyjs.com/cloud/):
-
-[<img src="https://www.gatsbyjs.com/deploynow.svg" alt="Deploy to Gatsby Cloud">](https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-minimal)
+![pdf page](./static/page-image.png)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,54 +2,44 @@ import React, { useState } from "react"
 import loadable from '@loadable/component'
 import { graphql } from "gatsby"
 import './index.css'
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export const frontmatter = {
-  title: "Choropleth on d3v4",
-  written: "2017-05-04",
-  layoutType: "post",
-  path: "../images/sample.pdf",
-  image: "../images/sunset-g1ab685e59_1280.jpg",
-  category: "data science",
-  description: "Things about the choropleth.",
+   title: "Choropleth on d3v4",
+   written: "2017-05-04",
+   layoutType: "post",
+   path: "../images/sample.pdf",
+   category: "data science",
+   description: "Things about the choropleth.",
 }
 
 const OtherComponent = loadable(() => import('../components/Component'))
 
 const IndexPage = (data) => {
-  const [isOpen, setIsOpen] = useState(false);
+   const [isOpen, setIsOpen] = useState(false);
 
-  const path = data.data.javascriptFrontmatter.frontmatter.path.publicURL;
-  const image = data.data.javascriptFrontmatter.frontmatter.image;
-  console.log("da", image)
+   const path = data.data.javascriptFrontmatter.frontmatter.path.publicURL;
 
-  const open = () => {
-    setIsOpen(true);
-  }
+   const open = () => {
+      setIsOpen(true);
+   }
 
-  const close = () => {
-    setIsOpen(false);
-  }
+   const close = () => {
+      setIsOpen(false);
+   }
 
-  return (
-    <main>
-      <GatsbyImage image={getImage(image)} alt="here" width={100} height={100} />
-      <h1>hello</h1>
-      <button onClick={open}>Open</button>
-      {isOpen && <OtherComponent file={path} close={close}/>}
-    </main>
-  )
+   return (
+      <main>
+         <h1>Click to open the PDF modal</h1>
+         <button onClick={open} style={{ padding: "1rem 2rem", borderRadius: "5px" }}>Open Modal</button>
+         {isOpen && <OtherComponent file={path} close={close} />}
+      </main>
+   )
 }
 
 export const query = graphql`
   {
     javascriptFrontmatter {
       frontmatter {
-        image {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
         path {
           publicURL
         }
